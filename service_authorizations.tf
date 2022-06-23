8c0afa368762c21ddae1d3a0acae9d59cf5c49d8
jvallexm <jvallexm@gmail.com>
2022-06-23 13:29:04 -0400

    Fix type delcaration

--- service_authorizations.tf
+++ service_authorizations.tf
@@ -6,7 +6,7 @@
 resource "ibm_iam_authorization_policy" "policy" {
   count                       = var.authorize_vpc_reader_role == true ? 1 : 0
   source_service_name         = "server-protect"
-  target_service_name         = local.key_management_type
+  target_service_name         = local.use_hs_crypto == true ? "hs-crypto" : "kms"
   target_resource_instance_id = local.key_management_guid
   roles                       = ["Reader"]
   description                 = "Allow block storage volumes to be encrypted by Key Management instance."

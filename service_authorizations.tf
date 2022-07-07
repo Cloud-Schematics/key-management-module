c4fbd561fd354c59e8f5032e3c7c9aef31d65281
jvallexm <jvallexm@gmail.com>
2022-07-07 14:02:02 -0400

    Fix depends on

--- service_authorizations.tf
+++ service_authorizations.tf
@@ -15,10 +15,10 @@ resource "ibm_iam_authorization_policy" "server_protect_policy" {
 resource "ibm_iam_authorization_policy" "block_storage_policy" {
   count                       = var.authorize_vpc_reader_role == true ? 1 : 0
   source_service_name         = "is"
-  source_resource_type        = "volume"
+  source_resource_type        = "share"
   target_service_name         = local.use_hs_crypto == true ? "hs-crypto" : "kms"
   target_resource_instance_id = local.key_management_guid
-  roles                       = ["Reader"]
+  roles                       = ["Reader", "Authorization Delegator"]
   description                 = "Allow block storage volumes to be encrypted by Key Management instance."
 }
 
